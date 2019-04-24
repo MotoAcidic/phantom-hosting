b01e2876b87a8c221a118711c1e3593404e15bb8
Jack Coble <jackkdev@protonmail.com>
2019-04-24 20:31:08 +0100

    Attempt to resolve failing build

--- config/config.go
+++ config/config.go
@@ -10,19 +10,19 @@ import (
 )
 
 type MasternodeString struct {
-	Alias            string `json:"alias"`
-	Genkey           string `json:"genkey"`
-	IPv4             string `json:"ipv6"`
-	Port             int    `json:"port"`
-	TransactionID    string `json:"txid"`
-	TransactionIndex int    `json:"tx_index"`
-	EpochTime        int64  `json:"epoch_time"`
+	Alias            uuid.UUID `json:"alias"`
+	Genkey           string    `json:"genkey"`
+	IPv4             string    `json:"ipv6"`
+	Port             int       `json:"port"`
+	TransactionID    string    `json:"txid"`
+	TransactionIndex int       `json:"tx_index"`
+	EpochTime        int64     `json:"epoch_time"`
 }
 
 func GenerateNodeDetails(m MasternodeString) (mnString string, err error) {
 	m.EpochTime = time.Now().Unix()
 	m.IPv4 = fake.IPv4()
-	m.Alias = uuid.NewV4().String()
+	m.Alias = uuid.NewV4()
 
 	if m.TransactionID == "" {
 		return "", errors.New("Transaction ID is required")
